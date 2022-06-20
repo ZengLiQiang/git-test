320df173f04394b6a35cde8dd01dd78056bf4476
zeng <1771685088@qq.com>
2022-06-20 14:39:19 +0800

    second commit

--- src/main/java/com/zeng/git/GitTest.java
+++ src/main/java/com/zeng/git/GitTest.java
@@ -10,5 +10,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git2!");
     }
 }
