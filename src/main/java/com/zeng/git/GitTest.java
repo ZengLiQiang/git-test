47da4134ff0c279863515027478fac5da3714ee1
zeng <1771685088@qq.com>
2022-06-20 14:28:27 +0800

    first commit

---
+++ src/main/java/com/zeng/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.zeng.git;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+/**
+ * @Author: zlq
+ * @DateTime: 2022/6/20 14:25
+ * @Description: TODO
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
